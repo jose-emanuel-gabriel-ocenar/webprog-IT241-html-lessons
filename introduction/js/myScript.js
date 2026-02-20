e8aff2804b4dceb08adf4382d06769d8528e5f09
jose-emanuel-gabriel-ocenar <ocenarjeggy@gmail.com>
2026-02-20 21:23:46 +0800

    personalized js

--- introduction/js/myScript.js
+++ introduction/js/myScript.js
@@ -1,3 +1,8 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+// Change About Me text
+function changeAbout() {
+  document.getElementById("aboutText").innerHTML =
+    "I am continuously improving my skills in web development and programming to become a future software developer.";
 }
+
+// Automatically display current year in footer
+document.getElementById("year").innerHTML = new Date().getFullYear();
